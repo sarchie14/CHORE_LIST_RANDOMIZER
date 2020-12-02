34f3413170c4d75aedc63da986e5b23ab61be9b5
sarchie14 <spencer.archibald05@gmail.com>
2020-12-02 00:34:27 -0600

    Added new_chore and list_chore functions. Also worked on more of the main function

--- CHORE_LIST_RANDOMIZER.py
+++ CHORE_LIST_RANDOMIZER.py
@@ -1,18 +1,6 @@
-#SUNDAY: COMMENT ON EACH MENU FUNCTION AND LIST WHAT EACH SHOULD EXPECT TO DO
-#		 BE SPECIFIC.
 #		 ALSO READ ON HOW TO SAVE THINGS TO A DOCUMENT.
-# READ IN THE CHORE LIST FROM THE DOC, AND SAVE THE CHORES/MEMBERS/ASSIGNED TO VARIABLES.
-
-###		1. ENTER NEW CHORE
-#				CHECK TO SEE IF EXISTING CHORE IS IN DOC.
-#						
-#		2. REMOVE CHORE
-#		3. ENTER NEW MEMBER
-#		5. REMOVE MEMBER
-#		6. LIST CHORES
-#		7. LIST MEMBERS
-#		8. ASSIGN MEMBER'S CHORES
-#		9. SAVE INTO DOCUMENT
+# READ IN THE CHORE LIST FROM A DOC.
+# Fix while statement in menu() where it will only accept y or n OR Y or N from user
 
 menu = """***************************\n
 		1. ENTER NEW CHORE\n
@@ -24,5 +12,44 @@ menu = """***************************\n
 		8. ASSIGN MEMBER'S CHORES\n
 		9. SAVE INTO DOCUMENT\n
 		***************************\n"""
-		
-print(menu)
+chores = []
+
+def new_chore(): 
+    global chores
+    i = 0
+    
+    print('Enter Chore -> ')
+    newChore = input()
+
+    chores = chores + [newChore]
+    print('\nChore entered successfully')
+
+def list_chores():
+    global chores
+
+    for x in chores:
+        print(x)
+
+
+def menu_selection(argument):
+    if argument == '1':
+        new_chore()
+    elif argument == '6':
+        list_chores()
+
+def main():
+
+    to_menu = True
+    while to_menu:
+        print(menu)
+
+        user_menu_selection = input()
+        menu_selection(user_menu_selection)
+
+        print('Would you like to go back to menu?')
+        
+        user_input_to_menu = input()
+
+        if user_input_to_menu != 'y':
+            to_menu = False
+main()    
